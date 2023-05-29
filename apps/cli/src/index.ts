df0a944bd73342afb882e73094bae5e19e5d36ff
Sarat Angajala <saratangajala@gmail.com>
2023-05-29 22:01:12 +0530

    cli setup

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,6 +1,20 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+
+const main = async () => {
+  const argv = await yargs(hideBin(process.argv))
+    .usage("Usage: $0 <command> [options]")
+    .command("serve", "Starts cryptobase and serves API endpoints").argv;
+
+  const [command] = argv._;
+
+  switch (command) {
+    case "serve":
+      console.log("In serve");
+      break;
+    default:
+      throw Error("No command given");
   }
-  return a + b;
 };
+
+main().catch((err: Error) => console.log(err));
--- apps/cli/test/blah.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "../src";
-
-describe("TSDX main", () => {
-  it("works", () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
